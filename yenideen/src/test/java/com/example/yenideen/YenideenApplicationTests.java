c6b05b92d1650f627d94ddf19c686d9d95420602
Ayşe Nur Arıkdoğan <aysenurarikdogan@gmail.com>
2026-09-08 17:54:47 +0300

    test: add unit tests for BookService, controller test for BookController and H2 test profile

--- yenideen/src/test/java/com/example/yenideen/YenideenApplicationTests.java
+++ yenideen/src/test/java/com/example/yenideen/YenideenApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.yenideen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class YenideenApplicationTests {
 
 	@Test
